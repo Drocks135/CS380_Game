768c6f2ed58cbb97945ee49bb27e614de1486522
Aaron Trudeau <trudeaua@mail.gvsu.edu>
2021-03-07 13:50:26 -0500

    Got rid of placeholder code in main

--- LeagueWindows/main.cpp
+++ LeagueWindows/main.cpp
@@ -42,10 +42,6 @@ int main(int argc, char** argv){
 
 	Player* player = new Player(&PlayerAnimationData);
 
-
-	/*auto player_up = [player](double delta) { player->up(delta); };
-	auto player_down = [player](double delta) { player->down(delta); };
-	auto player_left = [player](double delta) { player->left(delta); };*/
 	auto player_up = [player](double delta, bool start) { player->up(delta, start); };
 	auto player_down = [player](double delta, bool start) { player->down(delta, start); };
 	auto player_left = [player](double delta, bool start) { player->left(delta, start); };
@@ -58,31 +54,6 @@ int main(int argc, char** argv){
 	one.addKeyEvent(SDLK_a, player_left);
 	one.addKeyEvent(SDLK_d, player_right);
 	one.addKeyEvent(SDLK_s, player_down);
-
-	/*// Make a banana and add to scene. Should update and draw.
-	SDL_Surface* surface = IMG_Load("../assets/banana.png");
-	//Banana* b = new Banana();
-	//Sprite* b = new Sprite(surface, 3);
-	//Sprite* other = new Sprite(surface, 1);
-	Banana* b = new Banana();
-	Banana* b2 = new Banana();
-	delete b2;
-
-	b->setLayer(2);
-
-
-	std::cout << "Result is: " << b->getLayer() << std::endl;
-	//Banana* b = new Banana();
-	one.addUpdateable(b);
-	one.addDrawable(b);
-	auto b_up = [b](double delta) { b->up(delta); };
-	auto b_down = [b](double delta) { b->down(delta); };
-	auto b_left = [b](double delta) { b->left(delta); };
-	auto b_right = [b](double delta) { b->right(delta); };
-	one.addKeyEvent( SDLK_w, b_up );
-	one.addKeyEvent( SDLK_a, b_left );
-	one.addKeyEvent( SDLK_d, b_right );
-	one.addKeyEvent( SDLK_s, b_down );*/
 
 	// Set the scene in the engine
 	engine.setScene(&one);
